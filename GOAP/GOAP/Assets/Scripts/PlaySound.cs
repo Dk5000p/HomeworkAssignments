5d74f2bfd8b6950b4b5a07f3c9e770804e0dd58c
Doug Kent <dk5000p@gmail.com>
2020-11-03 22:22:55 -0500

    GOAP project

---
+++ GOAP/GOAP/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : GAction
+{
+    public AudioSource sound;
+    public override bool PrePerform()
+    {
+
+        return true;
+    }
+
+    public override bool PostPerform()
+    {
+      
+            sound.Play();
+            
+        
+        
+        return true;
+    }
+}
---
+++ GOAP/GOAP/Assets/Scripts/Register.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public class Register : GAction {
+    public override bool PrePerform() {
+
+        return true;
+    }
+
+    public override bool PostPerform() {
+        DialogueManager.scc.setGameStateValue("playerWearing", "equals", "shirt");
+        Debug.Log(DialogueManager.scc.getSCCLine("Patient"));
+        return true;
+    }
+}
